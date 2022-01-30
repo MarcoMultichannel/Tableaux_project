9511223c0cdca8326ffcf41bafaeeb25448efb25
Marco <marcomennillo97@gmail.com>
2022-01-30 12:02:00 +0100

    First commit

--- src/main/java/TableauxMain.java
+++ src/main/java/TableauxMain.java
@@ -6,6 +6,6 @@ import java.io.File;
 public class TableauxMain {
     public static void main(String[] args) throws OWLOntologyCreationException {
         final OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        final OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File("/home/galigator/myLocalDir/aura.owl"));
+        final OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File("TODO"));
     }
 }
