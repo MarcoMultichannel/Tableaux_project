2bd96a036389d0066f1dcc0435930b6a3d5fbedc
Marco <marcomennillo97@gmail.com>
2022-01-31 13:31:20 +0100

    MyOWLPaser created Main class just for testing the owl-api

---
+++ src/main/java/MyOWLParser.java
@@ -0,0 +1,97 @@
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.model.parameters.Imports;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyOWLParser {
+    final OWLOntologyManager manager;
+    public MyOWLParser(){
+        this.manager=OWLManager.createOWLOntologyManager();
+    }
+    public OWLOntology loadOntologyFromFile(String path) throws OWLOntologyCreationException {
+        return manager.loadOntologyFromOntologyDocument(new File(path));
+    }
+    public OWLOntology loadOntologyFromString(String inputStream){
+        //TODO
+        return null;
+    }
+    public List<OWLEquivalentClassesAxiom> getEquivalentClassesAxioms(OWLOntology ont){
+        return new ArrayList<>(ont.getAxioms(AxiomType.EQUIVALENT_CLASSES,Imports.EXCLUDED));
+        //TODO che vuol dire Imports.EXCLUDED?
+    }
+    public List<OWLSubClassOfAxiom> getSubClassAxioms(OWLOntology ont){
+        return new ArrayList<>(ont.getAxioms(AxiomType.SUBCLASS_OF,Imports.EXCLUDED));
+        //TODO che vuol dire Imports.EXCLUDED?
+    }
+    public List<OWLClassExpression> unpackEquilvalentClassesAxiom(OWLEquivalentClassesAxiom axiom) throws OWLException {
+        if(axiom.getAxiomType()!=AxiomType.EQUIVALENT_CLASSES){
+            throw new OWLException("Not EQUIVALENT_CLASSES");
+        }
+        return new ArrayList<>(axiom.getClassExpressions());
+    }
+    public List<OWLClassExpression> unpackSubClassAxioms(OWLSubClassOfAxiom axiom) throws OWLException {
+        if(axiom.getAxiomType()!=AxiomType.SUBCLASS_OF){
+            throw new OWLException("Not SUBCLASS_OF");
+        }
+        ArrayList<OWLClassExpression> array=new ArrayList<>();
+        array.add(axiom.getSubClass());
+        array.add(axiom.getSuperClass());
+        return array;
+    }
+    public List<OWLClassExpression> unpackIntersection(OWLClassExpression and) throws OWLException {
+        if(and.getClassExpressionType()!= ClassExpressionType.OBJECT_INTERSECTION_OF){
+            throw new OWLException("Not an OBJECT_INTERSECTION_OF");
+        }
+        return new ArrayList<>(((OWLObjectIntersectionOf)and).getOperands());
+    }
+    public List<OWLClassExpression> unpackUnion(OWLClassExpression or) throws OWLException {
+        if(or.getClassExpressionType()!= ClassExpressionType.OBJECT_UNION_OF){
+            throw new OWLException("Not an OBJECT_UNION_OF");
+        }
+        return new ArrayList<>(((OWLObjectUnionOf)or).getOperands());
+    }
+    public OWLClassExpression getExistClassExpression(OWLClassExpression exists) {
+        OWLObjectSomeValuesFrom ce= (OWLObjectSomeValuesFrom) exists;
+        return ce.getFiller();
+    }
+    public OWLClassExpression getForeachClassExpression(OWLClassExpression foreach) {
+        OWLObjectAllValuesFrom ce= (OWLObjectAllValuesFrom) foreach;
+        return ce.getFiller();
+    }
+    public OWLObjectPropertyExpression getExistRole(OWLClassExpression exists) throws OWLException {
+        OWLObjectSomeValuesFrom ce= (OWLObjectSomeValuesFrom) exists;
+        OWLObjectPropertyExpression propExpr=ce.getProperty();
+        if(propExpr.isOWLObjectProperty())
+            return propExpr;
+        else
+            throw new OWLException("This is not ALC logic");
+    }
+    public OWLObjectPropertyExpression getForeachRole(OWLClassExpression exists) throws OWLException {
+        OWLObjectAllValuesFrom ce= (OWLObjectAllValuesFrom) exists;
+        OWLObjectPropertyExpression propExpr=ce.getProperty();
+        if(propExpr.isOWLObjectProperty())
+            return propExpr;
+        else
+            throw new OWLException("This is not ALC logic");
+    }
+
+//isSomething methods
+    public boolean isIntersection(OWLClassExpression ce){
+        return ce.getClassExpressionType() == ClassExpressionType.OBJECT_INTERSECTION_OF;
+    }
+    public boolean isUnion(OWLClassExpression ce){
+        return ce.getClassExpressionType() == ClassExpressionType.OBJECT_UNION_OF;
+    }
+    public boolean isExists(OWLClassExpression ce){
+        return ce.getClassExpressionType() == ClassExpressionType.OBJECT_SOME_VALUES_FROM;
+    }
+    public boolean isForeach(OWLClassExpression ce){
+        return ce.getClassExpressionType() == ClassExpressionType.OBJECT_ALL_VALUES_FROM;
+    }
+    public boolean isClass(OWLClassExpression ce){
+        return ce.isOWLClass();
+    }
+}
--- src/main/java/TableauxMain.java
+++ src/main/java/TableauxMain.java
@@ -1,11 +1,69 @@
-import org.semanticweb.owlapi.apibinding.*;
 import org.semanticweb.owlapi.model.*;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class TableauxMain {
-    public static void main(String[] args) throws OWLOntologyCreationException {
-        final OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        final OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File("TODO"));
+    public static final String terminologyPath="ontology/T.owl";
+    public static final String conceptPath="ontology/C.owl";
+
+    public static void main(String[] args) throws OWLException {
+        MyOWLParser parser=new MyOWLParser();
+        OWLOntology conceptOntology= null;
+        try {
+            conceptOntology = parser.loadOntologyFromFile(conceptPath);
+        } catch (OWLOntologyCreationException e) {
+            e.printStackTrace();
+        }
+        List<OWLEquivalentClassesAxiom> axioms=parser.getEquivalentClassesAxioms(conceptOntology);
+        if(axioms.isEmpty())
+            return;
+        OWLClassExpression C=null;
+        try {
+            if(axioms.size()==1) {
+                C=parser.unpackEquilvalentClassesAxiom(axioms.get(0)).get(1);
+            }
+        } catch (OWLException e){
+        }
+        //TABLEAUX TEST
+        List<OWLClassExpression> L=new ArrayList<>();
+        Set<OWLClassExpression> temp_set=new HashSet<>();
+        L.add(C);
+        System.out.println(L);
+        if(parser.isIntersection(C)) {//AND
+            try {
+                L.addAll(parser.unpackIntersection(C));
+                System.out.println(L);
+            } catch (OWLException e) {
+            }
+        }
+        for(OWLClassExpression ce:L){
+            if(parser.isUnion(ce)){ //OR
+                temp_set.addAll(parser.unpackUnion(ce));
+            }
+        }
+        L.addAll(temp_set);
+        temp_set.clear();
+        System.out.println(L);
+        for(OWLClassExpression ce:L){
+            if(parser.isExists(ce)){ //ESISTE
+                temp_set.add(parser.getExistClassExpression(ce));
+                System.out.println(parser.getExistRole(ce));
+            }
+        }
+        L.addAll(temp_set);
+        temp_set.clear();
+        System.out.println(L);
+        for(OWLClassExpression ce:L){
+            if(parser.isForeach(ce)){ //PEROGNI
+                temp_set.add(parser.getForeachClassExpression(ce));
+                System.out.println(parser.getForeachRole(ce));
+            }
+        }
+        L.addAll(temp_set);
+        temp_set.clear();
+        System.out.println(L);
     }
 }
